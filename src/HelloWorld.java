258f7dc800494d1b6f86431a2c9565ec9a69318a
Thomas Servuc <thomas.selle@orange.fr>
2015-10-20 16:38:41 +0200

    LOL

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String [] args)
 	{
-		System.out.println("H3110 W0r1d ! :D");
+		System.out.println("H3110 W0r1d ! :D CIMER : JACKIE ET MICHEL");
 	}
 }
